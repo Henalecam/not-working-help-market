7170298aae26ba917b7c3e9a3cd93038b4a6f31b
Henrique Alexandre <Henriquealexandredec@gmail.com>
2023-05-28 12:35:05 -0300

    Metente

--- src/Main.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export function Main() {
-  return <div>Hello World!</div>;
-} 
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,64 @@
+const path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
+const HtmlPlugin = require('html-webpack-plugin')
+const tailwindcss = require('tailwindcss')
+const autoprefixer = require('autoprefixer')
+
 module.exports = {
   mode: "development",
-  entry: "./src/Main.tsx",
+  devtool: 'cheap-module-source-map',
+  entry: {
+    popup: path.resolve('src/popup/index.tsx'),
+  },
   module: {
     rules: [
       {
         use: "ts-loader",
         test: /\.tsx$/,
         exclude: /node_modules/,
-  }]
+      },
+      {
+      
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders : 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                ident: 'postcss',
+                plugins : [tailwindcss, autoprefixer],
+              },
+            },
+          },
+        ],
+      },
+           ]
   },
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: path.resolve('src/manifest.json'), to: path.resolve('dist') },
+        { from: path.resolve('src/static'), to: path.resolve('dist') },
       ],
       }),
+    new HtmlPlugin({
+      title: 'Help Market',
+      filename: 'popup.html',
+      chunks: ['popup'],
+    }),
+    
     ],
   resolve: {
     extensions: [".tsx", ".ts", ".js"]
   },
   output: {
-    filename: "index.js",
+    filename: '[name].js',
   }
 
 }
